ca07d78fd629a2d53db324ca75dde57eb909b179
trevorknottdev <tknott@autopay.com>
2017-05-09 11:16:52 -0600

    Shell Scripts - Bash Swag

---
+++ swaggin_bash_profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PS1="\[\033[1;96m\](\$(git branch 2>/dev/null | grep '^*' | colrm 1 2))
+︻デ═一 \e[m"
+
+
+hname=`hostname`
+echo "Welcome on $hname."
+
+echo -e "Kernal Details: " `uname -smr`
+echo -e "`bash --version`"
+echo -ne "Uptime: "; uptime
+echo -ne "Server time: "; date
